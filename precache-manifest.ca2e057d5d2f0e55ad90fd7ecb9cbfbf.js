37ece47e77a685ef5e0b55ecb5765e7d0970805f
88IO <8810aoba@gmail.com>
2019-12-22 17:04:48 +0900

    Updates

---
+++ precache-manifest.ca2e057d5d2f0e55ad90fd7ecb9cbfbf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cb1d3be739986cf6aacf504ba3c65bd",
+    "url": "/react-bulletin-board/index.html"
+  },
+  {
+    "revision": "66f8bf50f6aaf057c4af",
+    "url": "/react-bulletin-board/static/js/2.735f17f4.chunk.js"
+  },
+  {
+    "revision": "a07ba61270c0fbdd3681d08b12bd2390",
+    "url": "/react-bulletin-board/static/js/2.735f17f4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "00e77e445f8cf21af240",
+    "url": "/react-bulletin-board/static/js/main.9ec61334.chunk.js"
+  },
+  {
+    "revision": "c50849dfa871914d4b89",
+    "url": "/react-bulletin-board/static/js/runtime-main.4fe3980f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ac53ffea49e92f3167f9ae9aa7a79e83.js"
+  "/react-bulletin-board/precache-manifest.ca2e057d5d2f0e55ad90fd7ecb9cbfbf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-bulletin-board/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
